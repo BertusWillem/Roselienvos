c95b3dbb7655df8cfbd05fd5f5ffa0bb69643638
remomangreetwaroe <remo@mangre.nl>
2017-12-19 08:43:29 +0100

    Contactform
    Beheer en verwijder functie

---
+++ beheer/contactform.beheer.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$page = "Contact";
+include 'header.beheer.php';
+include '../includes/dbh.php';
+?>
+
+<html>
+<body>
+<table>
+
+<?php
+	$db = new PDO('mysql:host=localhost;dbname=roselienvos', 'root', '');
+
+	//selecteer alle reacties met id, naam, email, inhoud en datum van de auteur
+	$query = 	"SELECT id, naam, email, inhoud, datum
+				FROM contactformulier
+				ORDER BY datum desc";
+	$result = $db->query($query);
+	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+		echo "<tr><td>van <a href='mailto:" . $row["email"] . "'>" . $row["naam"] . "</a></td>
+			  <td>op " . $row["datum"]  . "</td></tr>\n";
+		echo "<tr><td colspan='2'>" . $row["inhoud"]  . "</td></tr>\n";
+		echo "<tr><td colspan='2'><a href='contactformverwijder.beheer.php?id=" . $row["id"]  . "'>
+		verwijder</a></td></tr>\n";
+		echo "<tr><td colspan='2'>&nbsp;</td></tr>\n";
+	}
+?>
+</table>
+</body>
+</html>
+
+<?php include ('../footer.php');
