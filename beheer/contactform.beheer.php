0d6163ba274213a2c80da2b3c9fdbb61bb74822d
remomangreetwaroe <remo@mangre.nl>
2017-12-19 10:09:44 +0100

    Knop
    Terugknop naar contactbeheer toegevoegd

--- beheer/contactform.beheer.php
+++ beheer/contactform.beheer.php
@@ -7,24 +7,34 @@ include '../includes/dbh.php';
 
 <html>
 <body>
-<table>
+ <section class="body-container">
+    <section class="container">
+        <div class="input-window">
+            <h1 style="background: white!important;">Terug naar contactbeheer</h1>
+            <a href="contact.beheer.php">
+              <div class="input-window" id="box" style="width: 100%!important; margin-bottom: 0;">
+                <input type="submit" value="Naar contactbeheer">
+        </div>
+				<table>
 
-<?php
-	//selecteer alle reacties met id, naam, email, inhoud en datum van de auteur
-	$query = 	"SELECT id, naam, email, inhoud, datum
-				FROM contactformulier
-				ORDER BY datum desc";
-	$result = $dbh->query($query);
-	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		echo "<tr><td>van <a href='mailto:" . $row["email"] . "'>" . $row["naam"] . "</a></td>
-			  <td>op " . $row["datum"]  . "</td></tr>\n";
-		echo "<tr><td colspan='2'>" . $row["inhoud"]  . "</td></tr>\n";
-		echo "<tr><td colspan='2'><a href='contactformverwijder.beheer.php?id=" . $row["id"]  . "'>
-		verwijder</a></td></tr>\n";
-		echo "<tr><td colspan='2'>&nbsp;</td></tr>\n";
-	}
-?>
-</table>
+				<?php
+					//selecteer alle reacties met id, naam, email, inhoud en datum van de auteur
+					$query = 	"SELECT id, naam, email, inhoud, datum
+								FROM contactformulier
+								ORDER BY datum desc";
+					$result = $dbh->query($query);
+					while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+						echo "<tr><td>van <a href='mailto:" . $row["email"] . "'>" . $row["naam"] . "</a></td>
+							  <td>op " . $row["datum"]  . "</td></tr>\n";
+						echo "<tr><td colspan='2'>" . $row["inhoud"]  . "</td></tr>\n";
+						echo "<tr><td colspan='2'><a href='contactformverwijder.beheer.php?id=" . $row["id"]  . "'>
+						verwijder</a></td></tr>\n";
+						echo "<tr><td colspan='2'>&nbsp;</td></tr>\n";
+					}
+				?>
+				</table>
+    </section>
+</section>
 </body>
 </html>
 
