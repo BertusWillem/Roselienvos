96595ba77d6bbcbaf4ea1ed6450cdb4741d1ed59
kevinvansloten <32487813+kevinvansloten@users.noreply.github.com>
2017-12-19 09:19:12 +0100

    contactbeheer fix

--- beheer/contact.beheer.php
+++ beheer/contact.beheer.php
@@ -2,26 +2,13 @@
 session_start();
 $page = "Contact";
 include 'header.beheer.php';
-include '../includes/dbh.php';
+include '../includes/paginalader.inc.php';
 
 
-$sth = $dbh->prepare("SELECT email FROM contactgegevens");
-            $sth -> execute();
-
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
-            $email=(implode($result));
-
-$sth = $dbh->prepare("SELECT telnummer FROM contactgegevens");
-            $sth -> execute();
-
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
-            $telnummer=(implode($result));
-
-$sth = $dbh->prepare("SELECT adres FROM contactgegevens");
-            $sth -> execute();
-
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
-            $adres=(implode($result));
+$result = contactgegeven($dbh);
+$email=($result["email"]);
+$telnummer=($result["telnummer"]);
+$adres=($result["adres"]);
 ?>
 
  <section class="body-container">
