addef605d975ea19ad2d0744f950c90fa562226c
Bas Klaassen <bwklaassen@outlook.com>
2017-12-20 11:28:59 +0100

    copyright uit footer gehaald

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
  <!--footer-->
     <footer>
-       &copy; <?php echo date("Y");?>
+      <!-- &copy; <?php echo date("Y");?> -->
+      <a>Deze site is gemaakt door: </a>
       <a class="user" href="http://google.nl/">Team Aqua.</a>
-      <a>All Rights Reserved.</a>
     </footer>
   </body>
 </html>
