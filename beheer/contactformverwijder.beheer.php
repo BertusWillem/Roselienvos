91f531a62acccf754b00dd2fc5eed661e61b5d60
remomangreetwaroe <remo@mangre.nl>
2017-12-20 16:45:49 +0100

    Commentaar bewerkt

--- beheer/contactformverwijder.beheer.php
+++ beheer/contactformverwijder.beheer.php
@@ -18,7 +18,7 @@ if ( isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT )){
   <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
   <strong>&#10004; De reactie is verwijderd!
 </div>';
-// Als het verwijderen van één bericht wel of niet is glukt, wordt de gebruiker automatisch weer teruggestuurd naar contactform.beheer.php, ook staat er een link om terug te keren
+// Als het verwijderen van één bericht wel glukt, wordt de gebruiker automatisch weer teruggestuurd naar contactform.beheer.php, ook staat er een link om terug te keren
 	echo '<meta http-equiv="Refresh" content="2;url=contactform.beheer.php"> <BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
 } else {
 		// Als het verwijderen van één bericht niet is gelukt, verschijnt er een Alert box met de melding dat het verwijderen niet is gelukt
@@ -26,6 +26,8 @@ if ( isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT )){
   <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
   <strong>&#88; Verwijderen van reactie is niet gelukt.
 </div>';
+// Als het verwijderen van één bericht niet is glukt, wordt de gebruiker automatisch weer teruggestuurd naar contactform.beheer.php, ook staat er een link om terug te keren
+	echo '<meta http-equiv="Refresh" content="2;url=contactform.beheer.php"> <BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
 }
 ?>
 
