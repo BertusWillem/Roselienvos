2678e1064fe5c0e3be725f04d60a705244a026b1
remomangreetwaroe <remo@mangre.nl>
2017-12-20 15:26:14 +0100

    commentaar

--- beheer/contactform.beheer.php
+++ beheer/contactform.beheer.php
@@ -42,20 +42,4 @@ include '../includes/dbh.php';
 </body>
 </html>
 
-<?php
-// Controleer of we daadwerkelijk een integer (geheel getal) hebben binnen gekregen
-if ( isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT )){
-	$id = $_GET['id'];
-
-	// Verbinding maken met de database
-
-	$query = "DELETE FROM contactformulier WHERE id = ? ";
-	$stmt = $dbh->prepare($query);
-	$stmt->execute(array($id));
-	echo '<div class="alert-box success"><span> </span>De reactie is verwijderd!</div><br  />';
-} else {
-	echo "Ongeldige aanvraag";
-}
-?>
-
 <?php include ('../footer.php');?>
