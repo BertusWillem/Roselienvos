771f363cc990402aec8713078d983d8113028dc5
remomangreetwaroe <remo@mangre.nl>
2017-12-20 22:09:25 +0100

    Redirect script aangepast
    Redirect script aangepast

--- beheer/contactformverwijder.beheer.php
+++ beheer/contactformverwijder.beheer.php
@@ -19,16 +19,19 @@ if ( isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT )){
   <strong>&#10004; De reactie is verwijderd!
 </div>';
 // Als het verwijderen van één bericht wel glukt, wordt de gebruiker automatisch weer teruggestuurd naar contactform.beheer.php, ook staat er een link om terug te keren
-	echo '<meta http-equiv="Refresh" content="2;url=contactform.beheer.php"> <BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
-} else {
+	echo '<BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
+// Script dat de gebruiker automatisch weer naar de contactform.beheer.php stuurt
+header("Refresh:2; url=contactform.beheer.php"); }
+	else {
 		// Als het verwijderen van één bericht niet is gelukt, verschijnt er een Alert box met de melding dat het verwijderen niet is gelukt
 	echo '<div class="alert">
   <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
   <strong>&#88; Verwijderen van reactie is niet gelukt.
 </div>';
 // Als het verwijderen van één bericht niet is glukt, wordt de gebruiker automatisch weer teruggestuurd naar contactform.beheer.php, ook staat er een link om terug te keren
-	echo '<meta http-equiv="Refresh" content="2;url=contactform.beheer.php"> <BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
-}
+	echo '<BR> <p style="text-align: center;">U wordt automatisch weer teruggestuurd, gebeurt er niks? <a href="contactform.beheer.php">Klik dan hier</a></p>';
+	// Script dat de gebruiker automatisch weer naar de contactform.beheer.php stuurt
+	header("Refresh:2; url=contactform.beheer.php"); }
 ?>
 
 <?php include ('../footer.php');?>
