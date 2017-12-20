570530768b425f3fcbc4c9f679497eedf3eb9be4
Bas Klaassen <bwklaassen@outlook.com>
2017-12-20 15:00:31 +0100

    css voor bericht overzicht

--- beheer/contactform.beheer.php
+++ beheer/contactform.beheer.php
@@ -14,25 +14,29 @@ include '../includes/dbh.php';
             <a href="contact.beheer.php">
               <div class="input-window" id="box" style="width: 100%!important; margin-bottom: 0;">
                 <input type="submit" value="Naar contactbeheer">
+              </div></a>
         </div>
-				<table>
 
-				<?php
-					//selecteer alle reacties met id, naam, email, inhoud en datum van de auteur
-					$query = 	"SELECT id, naam, email, inhoud, datum
-								FROM contactformulier
-								ORDER BY datum desc";
-					$result = $dbh->query($query);
-					while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-						echo "<tr><td>van <a href='mailto:" . $row["email"] . "'>" . $row["naam"] . "</a></td>
-							  <td>op " . $row["datum"]  . "</td></tr>\n";
-						echo "<tr><td colspan='2'>" . $row["inhoud"]  . "</td></tr>\n";
-						echo "<tr><td colspan='2'><a href='contactformverwijder.beheer.php?id=" . $row["id"]  . "'>
-						verwijder</a></td></tr>\n";
-						echo "<tr><td colspan='2'>&nbsp;</td></tr>\n";
-					}
-				?>
-				</table>
+        <div class="recensie-container">
+    				<?php
+    					//selecteer alle reacties met id, naam, email, inhoud en datum van de auteur
+    					$query = 	"SELECT id, naam, email, inhoud, datum FROM contactformulier ORDER BY datum desc";
+    					$result = $dbh->query($query);
+    					while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    						echo ("
+                  <div class='recensies' id='box' style='color: black!important;'>
+                    <table>
+                    <h1></h1>
+                    <tr><td>van:</td><td>".$row["naam"]."</td></tr>
+                    <tr><td>bericht:</td><td>" . $row["inhoud"]  . "</td></tr>
+                    <tr><td>op:</td><td>" . $row["datum"]  . "</td></tr>
+                    <tr><td id='fout'><a href='contactformverwijder.beheer.php?id=" . $row["id"]  . "'>verwijder</a></td><td id='goed'><a href='mailto:" . $row["email"] . "'>beantwoorden</td></tr>
+                    </table>
+                  </div>
+                ");
+    					}
+    				?>
+      </div>
     </section>
 </section>
 </body>
