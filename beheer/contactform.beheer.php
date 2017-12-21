6cbbcfeded7a569fa139782f506361aafe04fbf8
remomangreetwaroe <remo@mangre.nl>
2017-12-21 10:10:11 +0100

    comment

--- beheer/contactform.beheer.php
+++ beheer/contactform.beheer.php
@@ -35,7 +35,8 @@ include '../includes/dbh.php';
                     </table>
                   </div>
                 ");
-    					} ?>
+    					}
+              ?>
       </div>
     </section>
 </section>
