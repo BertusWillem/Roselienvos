cde97bc4203a780a6d4312539788d3f44656f0e4
kevinvansloten <32487813+kevinvansloten@users.noreply.github.com>
2018-01-07 16:52:06 +0100

    reciensie fix v3.0

--- includes/recensie.inc.php
+++ includes/recensie.inc.php
@@ -7,6 +7,12 @@ $rate = $_POST['rate'];
 $note = $_POST['toelichting'];
 $date = date("Y/m/d");
 include 'dbh.php';
+function validate($data) { //Filtert de input
+  $data = trim($data); //Haalt Spaties voor en achter weg uit de input
+  $data = stripslashes($data); //haalt backslash weg uit de input
+  $data = htmlspecialchars($data); //Convert speciale leesttekens naar HTML code
+  return $data; //stuurt gefilterde input terug
+}
 
 //De code hieronder heeft te maken met de Google captcha.
 $captcha = $_POST['g-recaptcha-response'];
@@ -31,16 +37,20 @@ if (intval($responseKeys["success"]) !== 1) {
         $_SESSION['re_note'] = $_POST['toelichting'];
         //Er worden session variablelen aangemaakt zodat de velden op de recensie pagina automatisch weer ingevuld worden
       }
-      if (preg_match("/([%\$#\*\>\<]+)/", $title) || preg_match("/([%\$#\<\>\*]+)/", $author) || preg_match("/([%\$#\<\>\*]+)/", $note))
-      {
-        header("Location: ../recensies.php?message=character");
-        $_SESSION['re_title'] = $_POST['titel'];
-        $_SESSION['re_name'] = $_POST['autheur'];
-        $_SESSION['re_rate'] = $_POST['rate'];
-        $_SESSION['re_note'] = $_POST['toelichting'];
+    //  if (preg_match("/([%\$#\*\>\<]+)/", $title) || preg_match("/([%\$#\<\>\*]+)/", $author) || preg_match("/([%\$#\<\>\*]+)/", $note))
+    //  {
+
+      //  header("Location: ../recensies.php?message=character");
+  //      $_SESSION['re_title'] = $_POST['titel'];
+    //    $_SESSION['re_name'] = $_POST['autheur'];
+    //    $_SESSION['re_rate'] = $_POST['rate'];
+    //    $_SESSION['re_note'] = $_POST['toelichting'];
         //Er worden session variablelen aangemaakt zodat de velden op de recensie pagina automatisch weer ingevuld worden
 
-      } else {
+    //  } else {
+    $title = validate($title);
+    $author = validate($author);
+    $note = validate($note);
 
     $stmt = $dbh->prepare("INSERT INTO recensie (titel, autheur, rate, toelichting, datum)
                            VALUES (:title, :author, :rate, :note, :date);");
@@ -53,4 +63,3 @@ if (intval($responseKeys["success"]) !== 1) {
     //Alle session variabelen worden geunset zodat de form weer leeg is.
     header("Location: ../recensies.php?message=succes");
     }
-}
